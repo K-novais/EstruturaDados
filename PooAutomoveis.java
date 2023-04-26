1db309a765e3908dc35754922ba0563fa13219fe
unknown <kellynamaral@yahoo.com>
2023-04-25 22:48:02 -0300

    first commit

---
+++ PooAutomoveis.java
@@ -0,0 +1,26 @@
+package EstruturaDados;
+
+public class PooAutomoveis {
+	
+	// declaração de atributos de classes
+	private String nomeProprietario;
+	private String modelo;
+	private String placa;
+	private int ano;
+	
+	// criar o metodo construtor
+	public PooAutomoveis(String nomeProprietario, String modelo, String placa, int ano) {
+		super();
+		this.nomeProprietario = nomeProprietario;
+		this.modelo = modelo;
+		this.placa = placa;
+		this.ano = ano;
+	}
+	
+	
+	
+	
+	
+	
+
+}
